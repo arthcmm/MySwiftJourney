8d7d739171f8788d45bf9ec0b75651311bc8f941
arthur :) <103451087+arthcmm@users.noreply.github.com>
2023-06-25 19:39:46 -0300

    Add files via upload

---
+++ Day 02/aula2Manha/aula2Manha/aula2Manha/aula2ManhaApp.swift
@@ -0,0 +1,17 @@
+//
+//  aula2ManhaApp.swift
+//  aula2Manha
+//
+//  Created by Student03 on 19/05/23.
+//
+
+import SwiftUI
+
+@main
+struct aula2ManhaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            desafio01Aula02()
+        }
+    }
+}
---
+++ Day 02/aula2Manha/aula2Manha/aula2Manha/desafio01Aula02.swift
@@ -0,0 +1,71 @@
+//
+//  desafio01Aula02.swift
+//  aula1Manha
+//
+//  Created by Student03 on 18/05/23.
+//
+
+import SwiftUI
+
+func meuFormatter() -> NumberFormatter {
+    let formatter = NumberFormatter()
+    formatter.numberStyle = .decimal
+    formatter.maximumFractionDigits = 2
+    return formatter
+}
+
+struct desafio01Aula02: View {
+    @State private var peso: Float = 0.00
+    @State private var tam: Float = 0.00
+    @State private var imc: Float = 0.00
+    @State private var bgColor = "white"
+    @State private var result = "Normal"
+
+    var body: some View {
+        ZStack{
+            VStack{
+                Text("Calculadora de IMC").font(.largeTitle)
+                TextField("Peso", value: $peso, formatter: meuFormatter())
+                    .multilineTextAlignment(.center)
+                    .keyboardType(.decimalPad)
+                    .textFieldStyle(RoundedBorderTextFieldStyle()).padding()
+                TextField("Altura", value: $tam, formatter: meuFormatter()).multilineTextAlignment(.center)
+                    .keyboardType(.decimalPad)
+                    .textFieldStyle(RoundedBorderTextFieldStyle()).padding()
+                Button("Calcular"){
+                    imc = (peso)/(pow(tam,2))
+                    switch(imc){
+                    case 0...18,5:
+                        result = "Baixo peso"
+                        bgColor = "baixoPeso"
+                        break
+                    case 18,6..<25:
+                        result = "Normal"
+                        bgColor = "normal"
+                        break
+                    case 25..<30:
+                        result = "Sobrepeso"
+                        bgColor = "sobrepeso"
+                        break
+                    case 30...:
+                        result = "Obesidade"
+                        bgColor = "obesidade"
+                        break
+                    default:
+                        break
+                    }
+                }.buttonStyle(.borderedProminent).controlSize(.large)
+                Spacer()
+                Text("\(result)").foregroundColor(.white).font(.title).bold()
+                Spacer()
+                Image("tabelaIMC").resizable().scaledToFit().shadow(radius: 5)
+            }.background(Color("\(bgColor)")).animation(.default, value: result)
+        }
+    }
+}
+
+struct desafio01Aula02_Previews: PreviewProvider {
+    static var previews: some View {
+        desafio01Aula02()
+    }
+}
